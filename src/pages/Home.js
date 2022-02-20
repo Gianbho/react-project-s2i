9b77f50cdae766837c125920472aa91725e51f29
Gianbho <gianluca.trm@gmail.com>
2022-02-20 16:08:56 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,6 @@ import RecipesList from '../components/RecipesList'
 import '../App.css'
 import LikedIngredientsForm from '../components/LikedIngredientsForm'
 import NotLikedIngredientsForm from '../components/NotLikedIngredientsForm'
-// REACT_APP_API_KEY = "ae096a3cdb31469f825397ed352cf9ac"
-// REACT_APP_API_KEY = "1ee2b871ce394004aa41b0cc65c61718"
-// REACT_APP_API_KEY = "f071caa85e3b452284e343e13ee1da2a"
-// REACT_APP_API_KEY = "08024b3c268a4224a8a6f87280e30b84"
 
 const Home = () => {
 
